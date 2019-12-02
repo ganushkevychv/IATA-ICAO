4102164ad6f7c045e4d5707f88b1bb23c845b1d5
Vladyslav <ganushkevychv@gmail.com>
2019-12-02 11:48:57 +0200

    search logic and dc.json file added to Delay Codes tab

--- alphabet/alphabet.js
+++ alphabet/alphabet.js
@@ -3,24 +3,24 @@ fetch("/alphabet/alphabet.json")
   .then(response => response.json())
   .then(json => (obj = json));
 
-searchFunc = () => {
-  let getResultDiv = document.getElementById("result");
-  let alphabet = document.getElementById("alphabet").value;
-  getResultDiv.innerHTML = "Result: " + Object.keys(obj[alphabet.toLowerCase()]);
-  if (alphabet in obj)
-  // checks if json is present and contains necessary values
-  emptyForm = () => {
+  searchFunc = () => {
+    let getResultDiv = document.getElementById("result");
     let alphabet = document.getElementById("alphabet").value;
-    if (alphabet === "") {
-      Swal.fire({
-        title: "Error!",
-        text: "You forgot to enter text",
-        icon: "error",
-        confirmButtonText: "OK"
-      });
-      return false;
-    }
-    return true;
+    getResultDiv.innerHTML = "Result: " + Object.keys(obj[alphabet.toLowerCase()]);
+    if (alphabet in obj)
+    // checks if json is present and contains necessary values
+    emptyForm = () => {
+      let alphabet = document.getElementById("alphabet").value;
+      if (alphabet === "") {
+        Swal.fire({
+          title: "Error!",
+          text: "You forgot to enter text",
+          icon: "error",
+          confirmButtonText: "OK"
+        });
+        return false;
+      }
+      return true;
+    };
+    emptyForm();
   };
-  emptyForm();
-};
---
+++ dc/dC.js
@@ -0,0 +1,26 @@
+let obj = {};
+fetch("/dc/dC.json")
+  .then(response => response.json())
+  .then(json => (obj = json));
+
+  searchFunc = () => {
+    let getResultDiv = document.getElementById("result");
+    let dC = document.getElementById("dC").value;
+    getResultDiv.innerHTML = "Result: " + Object.entries(obj[dC.toLowerCase()]);
+    if (dC in obj)
+    // checks if json is present and contains necessary values
+    emptyForm = () => {
+      let dC = document.getElementById("dC").value;
+      if (dC === "") {
+        Swal.fire({
+          title: "Error!",
+          text: "You forgot to enter text",
+          icon: "error",
+          confirmButtonText: "OK"
+        });
+        return false;
+      }
+      return true;
+    };
+    emptyForm();
+  };
