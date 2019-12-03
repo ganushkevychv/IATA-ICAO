47515d0da9d3ac9c9a5276f81b82036957d377b4
Vladyslav <ganushkevychv@gmail.com>
2019-12-03 15:35:48 +0200

    store user files to local storage completed

--- schedule/simple.js
+++ schedule/simple.js
@@ -1,24 +1,21 @@
-(async () => {
-    const loadingTask = PDFJS.getDocument("/schedule/crwsche.pdf");
-    const pdf = await loadingTask.promise;
-  
-    // Load information from the first page.
-    const page = await pdf.getPage(1);
-  
-    const scale = 1;
-    const viewport = page.getViewport(scale);
-  
-    // Apply page dimensions to the <canvas> element.
-    const canvas = document.getElementById("pdf");
-    const context = canvas.getContext("2d");
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-  
-    // Render the page into the <canvas> element.
-    const renderContext = {
-      canvasContext: context,
-      viewport: viewport
-    };
-    await page.render(renderContext);
-    console.log("Page rendered!");
-  })();
+(function() {
+  let FILE_KEY = 'crwsche.pdf';
+ 
+  document.querySelector('#fileUpload').addEventListener('change', handleFileUpload, false);
+ 
+  let reader = new FileReader();
+  reader.onload = handleFileRead;
+
+  function handleFileUpload(event) {
+    var file = event.target.files[0];
+    reader.readAsText(file);
+  }
+
+  function handleFileRead(event) {
+    var save = event.target.result
+    window.localStorage.setItem(FILE_KEY,save);
+  }
+ /*function retrieveSave() {
+    return localStorage.getItem(FILE_KEY)
+  }*/
+})();
