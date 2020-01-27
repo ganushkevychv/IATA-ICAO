6f214324c070cfd7e9365113ceadd9bddd34f389
Vladyslav <ganushkevychv@gmail.com>
2020-01-27 14:20:58 +0200

    sby page added to a iata project

--- sby/sby.js
+++ sby/sby.js
@@ -1,7 +1,3 @@
-/*let obj = {};
-fetch("/sby/sby.json")
-  .then(response => response.json())
-  .then(json => (obj = json));*/
 
   searchFunc = () => {
     let getResultDiv = document.getElementById("result");
@@ -11,24 +7,6 @@ fetch("/sby/sby.json")
     console.log(end)
     let result = end - start;
     console.log(result)
-   
-    getResultDiv.innerHTML = "time: " + result / 4 + " minutes"    
+    getResultDiv.innerHTML = "time: " + result / 4 + " minutes"
     
-    
-    
-    
-    /*emptyForm = () => {
-      let standBy = document.getElementById("standBy").value;
-      if (standBy === "") {
-        Swal.fire({
-          title: "Error!",
-          text: "You forgot to enter text",
-          icon: "error",
-          confirmButtonText: "OK"
-        });
-        return false;
-      }
-      return true;
-    };
-    emptyForm();*/
-  };
+  }   
